91194e176c2dc114cf9cf04ca2a962c34ea7b044
sokcheaitcoputer122 <sokcheaitcoputer122@gmail.com>
2025-10-25 21:04:17 +0700

    fix-bug-25-10-2025

--- parents/resources/js/Layouts/Nav/index.js
+++ parents/resources/js/Layouts/Nav/index.js
@@ -2,6 +2,5 @@ import user from '@/Layouts/Nav/user.js'
 import dashboard from '@/Layouts/Nav/dashboard'
 import position from '@/Layouts/Nav/position'
 import employee from '@/Layouts/Nav/employee'
-import stock from '@/Layouts/Nav/stock'
 
-export default [...dashboard,...user,...position,...employee,...stock]
+export default [...dashboard,...user,...position,...employee]
